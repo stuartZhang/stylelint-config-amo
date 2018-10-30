ad233ed367d4dcfdd9de13570b5b5d78f7949774
stuart_zhang <stuartpage@qq.com>
2018-10-30 18:34:21 +0800

    升级 eslint 依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,6 @@
 module.exports = {
-  'parserOptions': {
-    'ecmaVersion': 5,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'globalReturn': true,
-      'impliedStrict': false,
-      'jsx': false,
-      'experimentalObjectRestSpread': false
-    }
-  },
-  'env': {
-    'node': true,
-    'es6': true
-  },
-  'extends': [
-    'eslint:recommended',
-    'amo/eslint-config-bestpractice.js',
-    'amo/eslint-config-errors.js',
-    'amo/eslint-config-es6.js',
-    'amo/eslint-config-node.js',
-    'amo/eslint-config-possibleerrors.js',
-    'amo/eslint-config-stylistic.js',
-    'amo/eslint-config-var.js'
-  ],
-  'parser': 'babel-eslint',
-  'root': true
+  'root': true,
+    'extends': [
+      'minxing/eslint-config-node7x.js'
+  ]
 };
